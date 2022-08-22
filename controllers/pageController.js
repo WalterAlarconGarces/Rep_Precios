2171ce2cac5e9b13c162cdd44e2aa7f2b3a05e4a
WalterAlarconGarces <walarcon@gmail.com>
2022-08-22 17:45:19 -0400

    archivos generales

---
+++ controllers/pageController.js
@@ -0,0 +1,20 @@
+const sequelize = require('sequelize');
+const data_base = require('../config/data_base'); //calls database configuration
+
+exports.IndexPage = async (req,res)=>{
+    try {
+        res.render("index", {});
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+exports.Time = async (req,res)=>{
+    try {
+        const [results, metadata] = await data_base.query("SELECT NOW()" );
+        
+        res.send(results);
+    } catch (error) {
+        console.log(error)
+    }
+}
